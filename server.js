1119b397db053b0f24a75f4c7589648a1585bbd8
Sebastian <sebastianfox81@gmail.com>
2021-12-23 21:24:45 -0500

    MongoDB setup/connection

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
-
+const connectDB = require('./config/db')
 const app = express();
+
+connectDB();
 
 app.get('/', (req, res) => {
   res.send('API running succesfully')
 })
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`)

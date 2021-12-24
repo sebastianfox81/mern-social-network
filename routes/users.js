63d436bbb9e78d8a395a4d7005047615ece3ed74
Sebastian <sebastianfox81@gmail.com>
2021-12-23 23:31:03 -0500

    Implemented new user POST request

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { check, validationResult} = require('express-validator')
+const bcrypt = require('bcryptjs');
+const gravatar = require('gravatar');
+
+const User = require('../models/User');
 
 // @route  GET api/users
 // @desc   Register User
@@ -10,13 +14,42 @@ router.post('/',
   check('name', 'Name is required').not().isEmpty(),
   check('email', 'Please include a valid email').isEmail(),
   check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 })
-], (req, res) => {
+],
+  async (req, res) => {
+
+  const { name, email, password } = req.body;
+
+  try {
+    let user = await User.findOne({ email });
+
+    if (user) {
+      return res.status(400).json({ errors: [ { msg: 'User already exists' } ] })
+    }
+
+    const avatar = gravatar.url(email, {
+      s: '200',
+      r: 'pg',
+      d: 'mm'
+    });
+
+    user = new User({ name, email, avatar, password});
+
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(password, salt);
+
+    await user.save();
+
+
+  } catch (err) {
+    console.log(err)
+  }
+
   const errors = validationResult(req);
   if ( !errors.isEmpty() ) {
     return res.status(400).json({ errors: errors.array() })
   }
   console.log(req.body)
-  res.send('User route')
+  res.send('User register!')
 })
 
 module.exports = router;

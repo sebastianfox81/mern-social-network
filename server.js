e19d1d9c73c475a15930bd32a1d3b6c2d9e1268b
Sebastian <sebastianfox81@gmail.com>
2021-12-23 21:37:34 -0500

    Create routes

--- server.js
+++ server.js
@@ -2,11 +2,18 @@ const express = require('express');
 const connectDB = require('./config/db')
 const app = express();
 
+// Connect DB
 connectDB();
 
 app.get('/', (req, res) => {
   res.send('API running succesfully')
 })
+
+// Define Routes
+app.use('/api/users', require('./routes/users'))
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/profile', require('./routes/profile'))
+app.use('/api/post', require('./routes/post'))
 
 const PORT = process.env.PORT || 5001;
 

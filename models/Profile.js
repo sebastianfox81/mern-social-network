3dcbcb67964064927f78ff6ab7d734663b222204
Sebastian <sebastianfox81@gmail.com>
2021-12-30 15:46:49 -0500

    Create Post model

--- models/Profile.js
+++ models/Profile.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const ProfileSchema = new mongoose.Schema({
   user: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
+    ref: 'User'
   },
   company: {
     type: String

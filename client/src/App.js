0e9a55ef2692d62f7faf5b0737f5d38aa5e96703
Sebastian <sebastianfox81@gmail.com>
2022-01-02 21:45:40 -0500

    Create links in Navbar component

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,26 @@
 import React, { Fragment } from 'react';
-import Navbar from './components/Navbar.jsx';
-import Landing from './components/Landing.jsx';
+import { BrowserRouter, Routes, Route, Switch } from 'react-router-dom';
+import Navbar from './components/Navbar.js';
+import Landing from './components/Landing.js';
+import Login from './components/Login.js';
+import Register from './components/Register.js';
 import './App.css';
 
 function App() {
 
   return (
+    <BrowserRouter>
     <Fragment>
-      <Navbar />
-      <Landing />
+    <Navbar />
+    <Routes>
+      <Route exact path="/" element={<Landing />} />
+      <Route path="login" element={<Login />} />
+      <Route path="register" element={<Register />} />
+    </Routes>
     </Fragment>
+
+  </BrowserRouter>
+
   )
 }
 
